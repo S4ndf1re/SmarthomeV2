a78d75bf5a670e3a2821e89999e2b08a3170ab30
S4ndf1re <jan.kleinmann2000@gmail.com>
2021-12-28 21:18:16 +0100

    Adding Default Container to Gui

--- main.go
+++ main.go
@@ -12,21 +12,9 @@ func main() {
 	log.SetOutput(os.Stdout)
 
 	guiHandler := gui.NewGui()
+	guiHandler.AddContainer(passwordChangeContainer())
 	container := scripting.NewScriptContainer("scripts", guiHandler)
 	container.RunAll().Wait()
-
-	//tempCont := gui.NewContainer("test", func(name string) { log.Printf("%s\n", name) })
-
-	//tempBtn := gui.NewButton("button1", "Click Me", func(name string) { log.Printf("button1 clicked by %s\n", name) })
-	//tempCont.Add(tempBtn)
-
-	//tempCheck := gui.NewCheckbox("check1", "Check Me")
-	//tempCheck.SetChangeCallback(func(username string, state bool) {
-	//	log.Printf("check1 clicked by %s. Status: %t\n", username, state)
-	//})
-	//tempCont.Add(tempCheck)
-
-	//guiHandler.AddContainer(tempCont)
 
 	if err := http.ListenAndServe(":8080", guiHandler); err != nil {
 		panic(err)
